9a520b8ff9e3471f4fd475b419d10fd06fd5f43a
Rajiv Nair <rajivrnair@gmail.com>
2015-11-23 19:23:24 +0530

    Added DB connection ping.

--- web/src/main/java/net/codebrewery/pensieve/application/Pensieve.java
+++ web/src/main/java/net/codebrewery/pensieve/application/Pensieve.java
@@ -1,23 +1,45 @@
 package net.codebrewery.pensieve.application;
 
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Environment;
 import net.codebrewery.pensieve.config.PensieveConfiguration;
+import net.codebrewery.pensieve.database.ConnectionTestDAO;
 import net.codebrewery.pensieve.health.PingHealthCheck;
 import net.codebrewery.pensieve.resources.PingResource;
+import org.skife.jdbi.v2.DBI;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Pensieve extends Application<PensieveConfiguration>{
 
+    public static final String POSTGRESQL = "postgresql";
+
     @Override
     public void run(PensieveConfiguration configuration, Environment environment) throws Exception {
-        // Do this for every resource? Ugh?
-        final PingResource ping = new PingResource(new AtomicLong());
+        DBI database = setupDatabase(configuration, environment);
+        setupResources(environment, database);
+        setupHealthChecks(environment);
+    }
+
+    private DBI setupDatabase(PensieveConfiguration configuration, Environment environment) {
+        final DBIFactory factory = new DBIFactory();
+        DataSourceFactory dataSourceFactory = configuration.getDataSourceFactory();
+        return factory.build(environment, dataSourceFactory, POSTGRESQL);
+    }
+
+    private void setupResources(Environment environment, DBI database) {
+        final ConnectionTestDAO connectionTestDAO = database.onDemand(ConnectionTestDAO.class);
+        final PingResource ping = new PingResource(new AtomicLong(), connectionTestDAO);
+
+        environment.jersey().register(ping);
+    }
+
+    private void setupHealthChecks(Environment environment) {
         final PingHealthCheck pingHealthCheck = new PingHealthCheck();
 
         environment.healthChecks().register("ping", pingHealthCheck);
-        environment.jersey().register(ping);
     }
 
     public static void main(String[] args) throws Exception {
--- web/src/main/java/net/codebrewery/pensieve/config/PensieveConfiguration.java
+++ web/src/main/java/net/codebrewery/pensieve/config/PensieveConfiguration.java
@@ -1,7 +1,25 @@
 package net.codebrewery.pensieve.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class PensieveConfiguration extends Configuration {
 
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory database) {
+        this.database = database;
+    }
 }
