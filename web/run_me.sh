13549770d5156cf396433117dd7f3afba05f3a82
Rajiv Nair <rajivrnair@gmail.com>
2015-11-23 19:50:42 +0530

    Added migrations to startup script

--- web/run_me.sh
+++ web/run_me.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Cleaning, building and jar-ing"
 ./gradlew clean shadowJar
 
+echo "Applying db migrations..."
+java -jar build/libs/pensieve-all.jar db migrate config/pensieve.yml
+
+echo "Starting up..."
 java -jar build/libs/pensieve-all.jar server config/pensieve.yml
