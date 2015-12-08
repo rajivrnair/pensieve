c070aa0b86e53e17a4b26d8c4cac51d28e067f50
Rajiv Nair <rnair@rnair.local>
2015-12-08 09:19:03 +0530

    Integrate with Guice DI

--- web/application/src/main/java/net/codebrewery/pensieve/health/PingHealthCheck.java
+++ web/application/src/main/java/net/codebrewery/pensieve/health/PingHealthCheck.java
@@ -1,11 +1,16 @@
 package net.codebrewery.pensieve.health;
 
-import com.codahale.metrics.health.HealthCheck;
+import com.hubspot.dropwizard.guice.InjectableHealthCheck;
 
-public class PingHealthCheck extends HealthCheck {
+public class PingHealthCheck extends InjectableHealthCheck {
 
     @Override
     protected Result check() throws Exception {
         return Result.healthy();
+    }
+
+    @Override
+    public String getName() {
+        return "PingHealthCheck";
     }
 }
