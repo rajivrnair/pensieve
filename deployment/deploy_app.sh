52e6114faaef54024107ee6630e192f92c357a2a
Rajiv Nair <rajivrnair@gmail.com>
2015-12-22 18:08:42 +0530

    Add deployment scripts

---
+++ deployment/deploy_app.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Stopping app container"
+docker ps | grep pensieve-app | awk '{print $1}' | xargs docker stop
+
+echo "Removing app container"
+docker ps | grep pensieve-app | awk '{print $1}' | xargs docker rm
+
+echo "Fetching latest image"
+docker pull rajivrnair/pensieve
+
+echo "Starting app"
+docker-compose up -d pensieve-app
