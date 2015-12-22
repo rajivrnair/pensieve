5851df8814621380dcb3161b762c1e7201c09463
Rajiv Nair <rajivrnair@gmail.com>
2015-12-22 15:36:07 +0530

    Removed unused imports

--- web/application/src/main/java/net/codebrewery/pensieve/resources/MemoriesResource.java
+++ web/application/src/main/java/net/codebrewery/pensieve/resources/MemoriesResource.java
@@ -6,15 +6,11 @@ import net.codebrewery.pensieve.database.MemoriesDAO;
 import net.codebrewery.pensieve.domain.Memory;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import java.util.List;
 import java.util.UUID;
 
